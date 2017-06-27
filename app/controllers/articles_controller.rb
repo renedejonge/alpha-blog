4cb3dfb113b0a5d903df18d8bd123a61cd79b007
R de Jonge <rene@ikbenfrits.nl>
2017-06-27 11:20:22 +0200

    Fix create, add flash and error messages, create show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,9 +9,34 @@ class ArticlesController < ApplicationController
   def create
     #create action receives form data from new view through the params hash
     #render plain: params[:article].inspect
-    @article = Article.new(article_params) #article_params is private method
-    @article.save #hier wordt het artikel naar de database geschreven
-    redirect_to article_path(@article)
+    
+    #course video 34
+    #@article = Article.new(article_params) #article_params is private method
+    #@article.save #hier wordt het artikel naar de database geschreven
+    #redirect_to article_path(@article) #article_path (show) niet articles_path (index)
+
+    #course video 36 aangepast
+    #@article = Article.new(article_params)
+    #opgeslagen = @article.save #hier daadwerkelijk opgeslagen
+    #if opgeslagen
+    #  redirect_to article_path(@article)
+    #else
+    #  render 'new'
+    #end
+
+    #course video 36
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    #Bij de redirect naar show wordt er een artikel meegegeven hetgeen een :id oplevert die ook in de url zichtbaar wordt
+    @article = Article.find(params[:id])
   end
 
   private
