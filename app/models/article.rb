6723186a94fa589bfc9ee19bbba9fb432b472edd
R de Jonge <rene@ikbenfrits.nl>
2017-07-06 13:19:09 +0200

    add association between user and articles and add debuggr view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,11 @@
 class Article < ActiveRecord::Base
+  belongs_to :user
+
+  # voorbeeld: titel in hoofdletters voordat deze wordt opgeslagen
+  # before_save { self.title = title.upcase }
+  # of korter, en zonder self
+  #before_save { title.upcase! }
   validates :title, presence: true, length: {minimum: 3, maximum: 50}
   validates :description, presence: true, length: { minimum: 10, maximum: 300}
+  validates :user_id, presence: true
 end
