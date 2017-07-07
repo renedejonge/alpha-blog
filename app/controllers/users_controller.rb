c30a73175af1ef281fef176f9be1da02132257ab
R de Jonge <rene@ikbenfrits.nl>
2017-07-07 09:05:12 +0200

    written create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,18 @@ class UsersController < ApplicationController
 
   def create
     debugger
+    @user = User.new(user_params)
+    @user.save
+    if @user.save
+      flash[:success] = "Welcome to Alpha Blog, #{@user.username}!"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
