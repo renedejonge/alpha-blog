2d8d1d9d55ecb70d16d6180f7b1bd4adb7a3297e
R de Jonge <rene@ikbenfrits.nl>
2017-07-07 09:44:06 +0200

    probleem gevonden en opgelost

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   has_many :articles
-  #before_save { self.email = email.downcase! } # slaat email nil op in de database
+  ###before_save { self.email = email.downcase! } slaat email nil op in de database ondanks onderstaande validatie
+  ###before_save { self.email = email.downcase } is wel goed
+  ###before_save { email.downcase! } is ook goed
+  before_save { self.email = email.downcase }
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
   
   # VALID_EMAIL_REGEX= moet boven de validates regel die deze gebruikt staan! Want anders uninitialized constant foutmelding!
