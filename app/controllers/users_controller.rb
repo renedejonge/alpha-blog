bebdfa65d14628b42ca947e9389bfd3ef2c3be83
R de Jonge <rene@ikbenfrits.nl>
2017-07-11 16:27:38 +0200

    create admin users and allow actions on other user's articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,6 +57,7 @@ class UsersController < ApplicationController
 
   def require_same_user
     if !logged_in? || current_user != @user
+    #if current_user != @user
       flash[:danger] = "You can only edit your own account"
       redirect_to root_path
     end
