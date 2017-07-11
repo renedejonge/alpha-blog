7d7415b0a49f3604b98cb0d5eebf86d5b5f0ac45
R de Jonge <rene@ikbenfrits.nl>
2017-07-11 15:15:24 +0200

    Sign in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to Alpha Blog, #{@user.username}!"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
@@ -60,5 +61,5 @@ class UsersController < ApplicationController
       redirect_to root_path
     end
   end
-  
+
 end
