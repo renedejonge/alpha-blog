a8835dbd9b93797c134da07c0ac43ccd80de86a5
R de Jonge <rene@ikbenfrits.nl>
2017-07-17 12:42:53 +0200

    Add categories model and tests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,8 @@
 class CategoriesController < ApplicationController
 
   def index
-    @categories = Category.all
+    #@categories = Category.all
+    @categories = Category.paginate(page: params[:page], per_page: 4)
   end
 
   def new
